19e5dd11ce6dd8d378738a102d0295118406a579
Brandon viry <38904808+brandonviry@users.noreply.github.com>
2020-10-22 02:30:25 +0400

    Update main.c

--- exo3.1/main.c
+++ exo3.1/main.c
@@ -9,23 +9,34 @@ typedef struct temps Temps;
 
 Temps saisieTemps(void)
 {
- struct temps t ;
+ struct temps t = {-2,-2,-2} ;
+ while (t.heures >=  24 || t.heures <= 0)
+ { 
+  printf("1-SAISIR UNE VALEUR ENTRE 00 - 59 (2 chiffres): ");
 scanf("%d", &t.heures);
-scanf("%d", &t.minutes);
+ }
+ while (t.minutes >=  59 || t.minutes <= 0)
+ { 
+   printf("2-SAISIR UNE VALEUR ENTRE 00 - 59 (2 chiffres): ");
+   scanf("%d", &t.minutes);
+ }
+ while (t.secondes <= 0 || t.secondes>=  59){
+printf("3-SAISIR UNE VALEUR ENTRE 00 - 59 (2 chiffres) ");
 scanf("%d", &t.secondes);
-
+} 
 
  return t;
 
 }
 void afficheTemps(struct temps t)
 {
-     printf("%d:%d: %d\n", t.heures, t.minutes, t.secondes);
+     printf("%2d:%2d:%2d\n", t.heures, t.minutes, t.secondes);
 }
 int main(void)
 {   Temps t;
     t=  saisieTemps();
     afficheTemps( t);
+    system("PAUSE");
 
 }
 
