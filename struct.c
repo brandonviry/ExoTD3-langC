a8e123d28b9de95cfbfdddaec34537091e6f7e8c
Brandon viry <38904808+brandonviry@users.noreply.github.com>
2020-10-21 15:19:07 +0400

    Create struct.c

---
+++ struct.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct identification
+{
+    char nom[100];
+    char prenom[100];
+    long date;
+    char classe[100];
+};
+typedef struct identification id ;
+
+
+int main(int argc, char *argv[])
+{
+   id hsk;
+   affiche(&hsk);
+   printf("%s %s %ld %s ",hsk.nom,hsk.prenom,hsk.date,hsk.classe);
+    
+    return 0;
+}
+void affiche(id * speudo)
+{
+(*speudo).nom ="viry";
+(*speudo).prenom ="brandon";
+(*speudo).date =3;
+(*speudo).classe ="2aL2Info";
+}
