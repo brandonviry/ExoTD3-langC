355b92e1209f8373f20eaf7e5caf9898c3cd7978
Brandon viry <38904808+brandonviry@users.noreply.github.com>
2020-10-21 14:16:13 +0400

    Update main.c

--- exo3.1/main.c
+++ exo3.1/main.c
@@ -1,11 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
-struct temps { int heures; int minutes; int secondes; };
+struct temps {
+ int heures;
+ int minutes;
+  int secondes; };
+// Afin de faciliter la manipulation du type struct temps // on creer un synonyme Temps a l’aide de la commande typedef
+typedef struct temps Temps;
 
-
-int main()
+Temps saisieTemps(void)
 {
- printf("Hello,World");  
- return 0;
+ struct temps t ;
+ t.heures=0;
+ t.minutes=5;
+ t.secondes=4747;
 
 }
+void afficheTemps(struct temps t)
+{
+     printf("%d:%d: %d\n", t.heures, t.minutes, t.secondes);
+}
+int main(void)
+{   Temps t;
+    saisieTemps();
+    afficheTemps( t.heures, t.minutes, t.secondes);
+
+}
+
+
